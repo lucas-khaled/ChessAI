e51104953bf5db9c5e5114d16e258c9ce76d2ee6
lucas-khaled <khaledbrugger3@gmail.com>
2023-09-22 19:30:26 -0300

    Creates Piece color and some handy methods/properties

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -2,7 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Piece 
+public enum PieceColor
 {
-    
+    White,
+    Black
+}
+
+public abstract class Piece : MonoBehaviour
+{
+    public PieceColor pieceColor;
+
+    protected Tile actualTile;
+    protected bool isCaptured;
+    protected int Row => actualTile.TilePosition.row;
+    protected int Column => actualTile.TilePosition.column;
+    protected bool IsWhite => pieceColor == PieceColor.White;
+
+    public abstract Move[] GetPossibleMoves();
+    public virtual void MoveTo(Tile tile) 
+    {
+        actualTile = tile;
+        actualTile.Occupy(this);
+    }
+
+    public void Capture() 
+    {
+        actualTile.DeOccupy();
+        actualTile = null;
+        isCaptured = true;
+    }
+
+    public bool IsEnemyPiece(Piece piece) 
+    {
+        return pieceColor != piece.pieceColor;
+    }
 }
