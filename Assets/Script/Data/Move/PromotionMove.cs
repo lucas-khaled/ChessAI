6514d0523416c3457889fbb34155197fbacffec8
lucas-khaled <khaledbrugger3@gmail.com>
2023-11-26 12:18:32 -0300

    Do promotion moves and makes pawn returns it

---
+++ Assets/Script/Data/Move/PromotionMove.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PromotionMove : Move
+{
+    private Piece promoteTo;
+
+    public PromotionMove(Tile from, Tile to, Piece piece, Piece promoteTo, Piece capture = null) : base(from, to, piece, capture)
+    {
+        this.promoteTo = promoteTo;
+    }
+}
