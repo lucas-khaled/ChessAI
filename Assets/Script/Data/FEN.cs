3a1a08744f720ee0ac5ad618f4010240b439a625
lucas-khaled <khaledbrugger3@gmail.com>
2024-04-01 19:51:56 -0300

    Move FEN class to data folder

---
+++ Assets/Script/Data/FEN.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class FEN
+{
+    public string[] positions;
+    public PieceColor pieceColor;
+    public string castlingString;
+    public string enPassantString;
+    public string halfMovesString;
+    public string fullMovesString;
+
+    public string fullString;
+    public string fullPositionsString;
+
+    public FEN(string fen) 
+    {
+        string[] fieldsSplited = fen.Split(' ');
+
+        fullPositionsString = fieldsSplited[0];
+        positions = fullPositionsString.Split('/');
+
+        SetPieceColor(fieldsSplited[1]);
+
+        castlingString = fieldsSplited[2];
+        enPassantString = fieldsSplited[3];
+        halfMovesString = fieldsSplited[4];
+        fullMovesString = fieldsSplited[5];
+
+        fullString = fen;
+    }
+
+    private void SetPieceColor(string colorString) 
+    {
+        pieceColor = (colorString == "w") ? PieceColor.White : PieceColor.Black;
+    }
+
+    public override string ToString()
+    {
+        return fullString;
+    }
+}
