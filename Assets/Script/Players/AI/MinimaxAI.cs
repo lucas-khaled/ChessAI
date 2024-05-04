f94750068b31efee3d7fe9af0f5eabf75ca76795
lucas-khaled <khaledbrugger3@gmail.com>
2024-05-04 12:43:39 -0300

    Change the minimax eavluation method

--- Assets/Script/Players/AI/MinimaxAI.cs
+++ Assets/Script/Players/AI/MinimaxAI.cs
@@ -1,77 +1,100 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+using Random = UnityEngine.Random;
 
 public class MinimaxAI : AIPlayer
 {
-    private uint maxDepth = 2;
+    private int maxDepth = 2;
     private EndGameChecker endGameChecker;
 
-    public MinimaxAI(GameManager manager, uint depth = 2) : base(manager) 
+    public MinimaxAI(GameManager manager, int depth = 2) : base(manager) 
     {
         maxDepth = depth;
         endGameChecker = new(null);
     } 
+
     protected override async Task<Move> CalculateMove()
     {
-        return GetBestMoveFrom(GetAllMoves(manager.environment), actualColor);
-    }
+        var env = manager.environment;
 
-    private Move GetBestMoveFrom(List<Move> moves, PieceColor actualColor) 
-    {
-        float bestScore = (actualColor == PieceColor.White) ? float.MinValue : float.MaxValue;
+        bool isWhite = actualColor == PieceColor.White;
+        float bestScore = isWhite ? float.MinValue : float.MaxValue;
         Move bestMove = null;
-        foreach (var move in moves)
-        {
-            float score = Evaluate(manager.environment, move);
 
-            Debug.Log($"Score of {score} for {move}");
+        var moves = GetAllMoves(env);
+
+        Debug.Log($"Evaluating {moves.Count} moves");
+        foreach(var move in moves) 
+        {
+            Environment newEnv = env.Copy();
+            newEnv.turnManager.DoMove(move);
+            float score = Minimax(newEnv, isWhite, maxDepth);
+
+            Debug.Log($"Evaluated {move} \nwith a score of {score}");
+
             if (IsBetterScoreThan(score, bestScore))
             {
-                bestScore = score;
                 bestMove = move;
+                bestScore = score;
             }
+            /*else if(score == bestScore) 
+            {
+                var chanceToChange = Random.Range(1, 100);
+
+                if(chanceToChange > 50) 
+                {
+                    bestMove = move;
+                    bestScore = score;
+                }
+            }*/
+            
         }
 
-        Debug.Log($"Choosed score {bestScore} for move {bestMove}");
+        Debug.Log($"Choosed {bestMove} \nas best with a score of {bestScore}");
         return bestMove;
     }
 
-    private float Evaluate(Environment environment, Move move, int depth = 1) 
-    {
-        Environment env = environment.Copy();
-        endGameChecker.SetEnvironment(env);
-        
-        PieceColor moveColor = move.piece.pieceColor;
-        env.turnManager.DoMove(move);
 
-        if (endGameChecker.IsCheckMate()) 
-            return (moveColor == actualColor) ? 1000 - depth : -1000 + depth;
-        
-        if (endGameChecker.HasDraw()) 
+    private float Minimax(Environment env, bool isMaximize, int depth) 
+    {
+        endGameChecker.SetEnvironment(env);
+
+        if (endGameChecker.IsCheckMate())
+            return (isMaximize) ? 1000 - depth : -1000 + depth;
+
+        if (endGameChecker.HasDraw())
             return 0;
 
-        if (depth < maxDepth)
-            return Evaluate(env, GetBestMoveFrom(GetAllMoves(env), env.turnManager.ActualTurn), depth+1);
+        if (depth == 0)
+            return GetHeuristicOnPosition(env);
 
-        return GetHeuristicOnPosition(env);
+        float bestScore = isMaximize ? float.MinValue : float.MaxValue;
+        foreach (var move in GetAllMoves(env)) 
+        {
+            Environment newEnv = env.Copy();
+            newEnv.turnManager.DoMove(move);
+
+            float score = Minimax(newEnv, !isMaximize, depth-1);
+
+            bestScore = isMaximize ? Mathf.Max(bestScore, score) : Mathf.Min(bestScore, score);
+        }
+
+        return bestScore;
     }
 
-    private bool IsBetterScoreThan(float newScore, float oldScore) 
+    private bool IsBetterScoreThan(float newScore, float oldScore)
     {
         return (actualColor == PieceColor.White) ? newScore > oldScore : newScore < oldScore;
     }
 
-    private float GetHeuristicOnPosition(Environment environment)
+        private float GetHeuristicOnPosition(Environment environment)
     {
-        return GetEuristicOnMaterial(environment);
+        return GetHeuristicOnMaterial(environment);
     }
 
-    private float GetEuristicOnMaterial(Environment environment)
+    private float GetHeuristicOnMaterial(Environment environment)
     {
         var pieces = environment.board.pieces;
 
@@ -81,34 +104,34 @@ public class MinimaxAI : AIPlayer
     private float QueenMaterialCount(List<Piece> pieces) 
     {
         var score = GetMaterialCount(pieces.Where(x => x is Queen).ToList(), 10);
-        Debug.Log($"Queen score {score}");
+        //Debug.Log($"Queen score {score}");
         return score;
     }
 
     private float RookMaterialCount(List<Piece> pieces) 
     {
         var score = GetMaterialCount(pieces.Where(x => x is Rook).ToList(), 5);
-        Debug.Log($"Rook score {score}");
+        //Debug.Log($"Rook score {score}");
         return score;
     }
 
     private float BishopMaterialCount(List<Piece> pieces)
     {
         var score = GetMaterialCount(pieces.Where(x => x is Bishop).ToList(), 3);
-        Debug.Log($"Bishop score {score}");
+        //Debug.Log($"Bishop score {score}");
         return score;
     }
 
     private float KnightMaterialCount(List<Piece> pieces)
     {
         var score = GetMaterialCount(pieces.Where(x => x is Knight).ToList(), 3);
-        Debug.Log($"Knight score {score}");
+        //Debug.Log($"Knight score {score}");
         return score;
     }
     private float PawnMaterialCount(List<Piece> pieces)
     {
         var score = GetMaterialCount(pieces.Where(x => x is Pawn).ToList(), 1);
-        Debug.Log($"Pawn score {score}");
+        //Debug.Log($"Pawn score {score}");
         return score;
     }
 
@@ -117,7 +140,7 @@ public class MinimaxAI : AIPlayer
         int wQnt = pieces.Count(x => x.pieceColor == PieceColor.White);
         int bQnt = pieces.Count(x => x.pieceColor == PieceColor.Black);
 
-        Debug.Log($"White Qnt {wQnt} and Black Qnt {bQnt}");
+        //Debug.Log($"White Qnt {wQnt} and Black Qnt {bQnt}");
         return weight * (wQnt - bQnt);
     }
 }
