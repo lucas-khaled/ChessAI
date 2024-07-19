468a2c9e39b314d4c9c17a0a2c783799c2f8082a
lucas-khaled <khaledbrugger3@gmail.com>
2024-07-18 21:29:41 -0300

    Create CenterControl Heuristic
    Just missing the controls for Queen

---
+++ Assets/Script/AI/Heuristics/CenterControlHeuristic.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterControlHeuristic : Heuristic
+{
+    PawnCenterControlHandler pawnHandler = new();
+    RookCenterControlHandler rookHandler = new();
+    QueenCenterControlHandler queenHandler = new();
+    BishopCenterControlHandler bishopHandler = new();
+    KnightCenterControlHandler knightHandler = new();
+
+    public CenterControlHeuristic(float weight) : base(weight)
+    {
+    }
+
+    public override float GetHeuristic(Environment environment)
+    {
+        float blackPoints = 0;
+        float whitePoints = 0;
+
+        foreach(var piece in environment.board.pieces) 
+        {
+            float points = 0;
+
+            if (piece is Queen)
+                points = queenHandler.GetControlAmount(piece);
+            else if (piece is Bishop)
+                points = bishopHandler.GetControlAmount(piece);
+            else if (piece is Knight)
+                points = knightHandler.GetControlAmount(piece);
+            else if (piece is Rook)
+                points = rookHandler.GetControlAmount(piece);
+            else if (piece is Pawn)
+                points = pawnHandler.GetControlAmount(piece);
+
+            if (piece.pieceColor != PieceColor.White)
+                blackPoints += points;
+            else
+                whitePoints += points;
+
+        }
+
+        return (whitePoints - blackPoints) * weight;
+    }
+
+    private interface IPieceCenterControlHandler 
+    {
+        float GetControlAmount(Piece piece);
+    }
+
+    private class PawnCenterControlHandler : IPieceCenterControlHandler
+    {
+        private float[,] blackPos = new float[8, 8] 
+        {
+            { 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 0.06f, 0.13f, 0.13f, 0.13f, 0.13f, 0.13f, 0.13f, 0.06f },
+            { 0.06f, 0.2f, 0.26f, 0.26f, 0.26f, 0.26f, 0.2f, 0.06f },
+            { 0.06f, 0.4f, 0.46f, 0.66f, 0.66f, 0.46f, 0.4f, 0.06f },
+            { 0.06f, 0.4f, 0.8f, 1f, 1f, 0.8f, 0.4f, 0.06f },
+            { 0.06f, 0.4f, 0.8f, 1f, 1f, 0.8f, 0.4f, 0.06f },
+            { 0.06f, 0.4f, 0.46f, 0.66f, 0.66f, 0.46f, 0.4f, 0.06f },
+            { 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        private float[,] whitePos = new float[8, 8] 
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0.06f, 0.4f, 0.46f, 0.66f, 0.66f, 0.46f, 0.4f, 0.06f },
+            { 0.06f, 0.4f, 0.8f, 1f, 1f, 0.8f, 0.4f, 0.06f },
+            { 0.06f, 0.4f, 0.8f, 1f, 1f, 0.8f, 0.4f, 0.06f },
+            { 0.06f, 0.4f, 0.46f, 0.66f, 0.66f, 0.46f, 0.4f, 0.06f },
+            { 0.06f, 0.2f, 0.26f, 0.26f, 0.26f, 0.26f, 0.2f, 0.06f },
+            { 0.06f, 0.13f, 0.13f, 0.13f, 0.13f, 0.13f, 0.13f, 0.06f },
+            { 1, 1, 1, 1, 1, 1, 1, 1 }  
+        };
+
+    public float GetControlAmount(Piece piece)
+        {
+            int row = piece.GetTile().TilePosition.row;
+            int column = piece.GetTile().TilePosition.column;
+
+            if (piece.pieceColor == PieceColor.White)
+                return whitePos[row, column];
+            else
+                return blackPos[row, column];
+        }
+    }
+
+    private class RookCenterControlHandler : MappedCenterControlHandler
+    {
+        protected override float[,] Map => new float[8, 8]
+        {
+            { 0.26f, 0.38f, 0.61f, 0.81f, 0.81f, 0.61f, 0.38f, 0.26f },
+            { 0.38f, 0.46f, 0.69f, 0.88f, 0.88f, 0.69f, 0.46f, 0.38f },
+            { 0.61f, 0.69f, 0.81f, 1, 1, 0.81f, 0.69f, 0.61f },
+            { 0.81f, 0.88f, 1, 1, 1, 1, 0.88f, 0.81f},
+            { 0.81f, 0.88f, 1, 1, 1, 1, 0.88f, 0.81f},
+            { 0.61f, 0.69f, 0.81f, 1, 1, 0.81f, 0.69f, 0.61f },
+            { 0.38f, 0.46f, 0.69f, 0.88f, 0.88f, 0.69f, 0.46f, 0.38f },
+            { 0.26f, 0.38f, 0.61f, 0.81f, 0.81f, 0.61f, 0.38f, 0.26f }
+        };
+    }
+
+    private class QueenCenterControlHandler : MappedCenterControlHandler
+    {
+        protected override float[,] Map => throw new System.NotImplementedException();
+    }
+
+    private class BishopCenterControlHandler : MappedCenterControlHandler
+    {
+        protected override float[,] Map => new float[,]
+        {
+            { 0.83f, 0.62f, 0.43f, 0.35f, 0.35f, 0.43f, 0.62f, 0.83f },
+            { 0.62f, 0.86f, 0.66f, 0.55f, 0.55f, 0.66f, 0.86f, 0.62f },
+            { 0.43f, 0.66f, 0.88f, 0.76f, 0.76f, 0.88f, 0.66f, 0.43f },
+            { 0.35f, 0.55f, 0.76f, 1, 1, 0.76f, 0.55f, 0.35f },
+            { 0.35f, 0.55f, 0.76f, 1, 1, 0.76f, 0.55f, 0.35f },
+            { 0.43f, 0.66f, 0.88f, 0.76f, 0.76f, 0.88f, 0.66f, 0.43f },
+            { 0.62f, 0.86f, 0.66f, 0.55f, 0.55f, 0.66f, 0.86f, 0.62f },
+            { 0.83f, 0.62f, 0.43f, 0.35f, 0.35f, 0.43f, 0.62f, 0.83f },
+        };
+    }
+
+    private class KnightCenterControlHandler : MappedCenterControlHandler
+    {
+        protected override float[,] Map => new float[,]
+        {
+            { 0.14f, 0.29f, 0.36f, 0.5f, 0.5f, 0.36f, 0.29f, 0.14f },
+            { 0.29f, 0.43f, 0.71f, 0.86f, 0.86f, 0.71f, 0.43f, 0.29f },
+            { 0.36f, 0.71f, 1f, 1f, 1f, 1f, 0.71f, 0.36f },
+            { 0.5f, 0.86f, 1f, 1f, 1f, 1f, 0.86f, 0.5f },
+            { 0.5f, 0.86f, 1f, 1f, 1f, 1f, 0.86f, 0.5f },
+            { 0.36f, 0.71f, 1f, 1f, 1f, 1f, 0.71f, 0.36f },
+            { 0.29f, 0.43f, 0.71f, 0.86f, 0.86f, 0.71f, 0.43f, 0.29f },
+            { 0.14f, 0.29f, 0.36f, 0.5f, 0.5f, 0.36f, 0.29f, 0.14f },
+        };
+    }
+
+    private abstract class MappedCenterControlHandler : IPieceCenterControlHandler
+    {
+        protected abstract float[,] Map { get; }
+
+        public float GetControlAmount(Piece piece)
+        {
+            int row = piece.GetTile().TilePosition.row;
+            int column = piece.GetTile().TilePosition.column;
+
+            return Map[row, column];
+        }
+    }
+}
--- Assets/Script/AI/Heuristics/SimplePositionHeuristic.cs
+++ Assets/Script/AI/Heuristics/SimplePositionHeuristic.cs
@@ -7,6 +7,7 @@ public class SimplePositionHeuristic : PositionHeuristic
     protected override List<Heuristic> heuristics => new List<Heuristic>()
     {
         new MaterialHeuristic(),
+        new CenterControlHeuristic(0.5f),
         new PawnStructureHeuristic(0.3f),
         new MobilityHeuristic(0.1f)
     };
