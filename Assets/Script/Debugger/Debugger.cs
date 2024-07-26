e30552cb3fb5728e65093df0ef8964763e1c8122
lucas-khaled <khaledbrugger3@gmail.com>
2024-07-26 20:05:15 -0300

    Creates a debugger and make it debug timers

---
+++ Assets/Script/Debugger/Debugger.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Debugger
+{
+    private static Dictionary<string, TimeInfo> timeDict = new();
+
+    public static void StartTimer(string timerTag) 
+    {
+        DateTime now = DateTime.Now;
+        if (timeDict.ContainsKey(timerTag)) 
+        {
+            timeDict[timerTag].firstMeasureTime = now;
+            timeDict[timerTag].lastMeasureTime = now;
+            return;
+        }
+
+        TimeInfo info = new TimeInfo()
+        {
+            firstMeasureTime = now,
+            lastMeasureTime = now
+        };
+
+        timeDict.Add(timerTag, info);
+    }
+
+    public static void StopTimer(string timerTag) 
+    {
+        if (timeDict.ContainsKey(timerTag) is false)
+        {
+            Debug.LogError($"No timer was initialized with the tag {timerTag}");
+            return;
+        }
+
+        timeDict[timerTag].lastMeasureTime = DateTime.Now;
+    }
+
+    public static void DebugTimer(string timerTag, string message = "", bool alsoStop = false) 
+    {
+        if (timeDict.ContainsKey(timerTag) is false) 
+        {
+            Debug.LogError($"No timer was initialized with the tag {timerTag}");
+            return;
+        }
+
+        if (alsoStop) 
+        {
+            StopTimer(timerTag);
+        }
+
+        Debug.Log(message + timeDict[timerTag].DifferenceMS);
+    }
+
+    private class TimeInfo 
+    {
+        public DateTime firstMeasureTime;
+        public DateTime lastMeasureTime;
+        public TimeSpan TimeDifference => lastMeasureTime - firstMeasureTime;
+        public double DifferenceMS => TimeDifference.TotalMilliseconds;
+    }
+}
