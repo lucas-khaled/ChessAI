c659e0e52f7e3283507784ca5c648bb5301dbe89
lucas-khaled <khaledbrugger3@gmail.com>
2024-11-17 14:07:52 -0300

    instantiates pieces in bitboard

--- Assets/Script/Test/BitBoardVisualizer/BitBoardVisualizer.cs
+++ Assets/Script/Test/BitBoardVisualizer/BitBoardVisualizer.cs
@@ -1,26 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class BitBoardVisualizer : MonoBehaviour
+[RequireComponent(typeof(PiecesSetup))]
+public class BitBoardVisualizer : MonoBehaviour, IGameManager
 {
     [SerializeField] private BoardStarter boardStarter;
     [SerializeField] private BitBoardUI boardUI;
     [SerializeField] private Color uiColor = Color.white;
+    [SerializeField] private string FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-    private Board board;
+    private List<BitBoardUI> bitBoardUIs = new List<BitBoardUI>();
+
+    public Board GameBoard { get; private set; }
+    public Board TestBoard { get; private set; }
+
+    public TurnManager TurnManager { get; private set; }
+    public EndGameChecker EndGameChecker { get; private set; }
+    public MoveChecker MoveChecker { get; private set; }
+    public ZobristHashManager HashManager { get; private set; }
+
+    private PiecesSetup piecesSetup;
+
+    private void Awake()
+    {
+        piecesSetup = GetComponent<PiecesSetup>();
+        piecesSetup.SetManager(this);
+
+        HashManager = new ZobristHashManager();
+        HashManager.InitializeHashes();
+    }
 
     void Start()
     {
-        board = boardStarter.StartNewBoard();
+        GameBoard = boardStarter.StartNewBoard();
 
         int index = 0;
-        foreach(var row in board.GetTiles()) 
+        foreach(var row in GameBoard.GetTiles()) 
         {
             foreach(var tile in row) 
             {
                 var boardUI = Instantiate(this.boardUI);
                 boardUI.Set(tile, index, uiColor);
                 index++;
+
+                bitBoardUIs.Add(boardUI);
             }
+        }
+
+        GameBoard.FENManager.SetupByFEN(new FEN(FEN), piecesSetup.InstantiatePiece, this);
+    }
+
+    public void SetBitBoard(Bitboard bitboard) 
+    {
+        foreach (var bitUI in bitBoardUIs) 
+        {
+            long operation = bitUI.tile.Bitboard.value & bitboard.value;
+            bool isActive = operation > 0;
+            bitUI.gameObject.SetActive(isActive);
         }
     }
 }
