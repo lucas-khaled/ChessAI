f9a4714273730f7c803607aa5738b17eba02586d
lucas-khaled <khaledbrugger3@gmail.com>
2024-11-20 14:22:04 -0300

    Add bishop bitboard calculation

--- Assets/Script/Pieces/Bishop.cs
+++ Assets/Script/Pieces/Bishop.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class Bishop : SlidingPieces
 {
     public Bishop(Board board) : base(board)
@@ -6,7 +8,31 @@ public class Bishop : SlidingPieces
 
     public override void GenerateBitBoard()
     {
-        throw new System.NotImplementedException();
+        GenerateAttackingSquaresBitBoard();
+        GenerateKingDangerBitBoard();
+    }
+
+    private void GenerateKingDangerBitBoard()
+    {
+        List<Tile> kingDangerTiles = new List<Tile>();
+
+        var diagonals = actualTile.GetDiagonalsByColor(pieceColor);
+
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.topRightDiagonals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.topLeftDiagonals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.downRightDiagonals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.downLeftDiagonals));
+
+        KingDangerSquares = AddTilesBitBoards(kingDangerTiles);
+    }
+
+    private void GenerateAttackingSquaresBitBoard()
+    {
+        List<Tile> attackingTiles = new List<Tile>();
+
+        attackingTiles.AddRange(GetDiagonalBlockedSquares());
+
+        AttackingSquares = AddTilesBitBoards(attackingTiles);
     }
 
     public override Move[] GetMoves()
