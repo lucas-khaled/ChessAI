585c7169fe76e16eb455a1a332e417f2bd943507
lucas-khaled <khaledbrugger3@gmail.com>
2024-11-20 14:31:24 -0300

    Generate Queen's bitboard

--- Assets/Script/Pieces/Queen.cs
+++ Assets/Script/Pieces/Queen.cs
@@ -8,7 +8,40 @@ public class Queen : SlidingPieces
 
     public override void GenerateBitBoard()
     {
-        throw new System.NotImplementedException();
+        GenerateAttackingSquaresBitBoard();
+        GenerateKingDangerBitBoard();
+    }
+
+    private void GenerateKingDangerBitBoard()
+    {
+        List<Tile> kingDangerTiles = new List<Tile>();
+
+        var diagonals = actualTile.GetDiagonalsByColor(pieceColor);
+        var verticals = actualTile.GetVerticalsByColor(pieceColor);
+        var horizontals = actualTile.GetHorizontalsByColor(pieceColor);
+
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.topRightDiagonals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.topLeftDiagonals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.downRightDiagonals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(diagonals.downLeftDiagonals));
+
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(verticals.backVerticals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(verticals.frontVerticals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(horizontals.leftHorizontals));
+        kingDangerTiles.AddRange(GetTilesFromCoordinates(horizontals.rightHorizontals));
+
+        KingDangerSquares = AddTilesBitBoards(kingDangerTiles);
+    }
+
+    private void GenerateAttackingSquaresBitBoard()
+    {
+        List<Tile> attackingTiles = new List<Tile>();
+
+        attackingTiles.AddRange(GetDiagonalBlockedSquares());
+        attackingTiles.AddRange(GetVerticalBlockedSquares());
+        attackingTiles.AddRange(GetHorizontalBlockedSquares());
+
+        AttackingSquares = AddTilesBitBoards(attackingTiles);
     }
 
     public override Move[] GetMoves()
