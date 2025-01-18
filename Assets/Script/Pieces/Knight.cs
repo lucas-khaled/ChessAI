88e5c22a2517c38eb7e0687b9e287d4a8e7b647e
lucas-khaled <khaledbrugger3@gmail.com>
2025-01-18 01:24:45 -0300

    fix knight attacking squares bitboard

--- Assets/Script/Pieces/Knight.cs
+++ Assets/Script/Pieces/Knight.cs
@@ -98,44 +98,76 @@ public class Knight : Piece
         //topLeft move
         int topLeftIndex = currentIndex + 15;
         if (currentIndex % 8 > 0 && topLeftIndex < 64)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(topLeftIndex));
+        {
+            var tile = Board.GetTileByIndex(topLeftIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //topRight move
         int topRightIndex = currentIndex + 17;
         if (currentIndex % 8 < 7 && topRightIndex < 64)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(topRightIndex));
+        {
+            var tile = Board.GetTileByIndex(topRightIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //leftTop move
         int leftTopIndex = currentIndex + 6;
         if (currentIndex % 8 > 1 && leftTopIndex < 64)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(leftTopIndex));
+        {
+            var tile = Board.GetTileByIndex(leftTopIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //leftDown move
         int leftDownIndex = currentIndex - 10;
         if (currentIndex % 8 > 1 && leftDownIndex >= 0)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(leftDownIndex));
+        {
+            var tile = Board.GetTileByIndex(leftDownIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //rightTop move
         int rightTopIndex = currentIndex + 10;
         if (currentIndex % 8 < 6 && rightTopIndex < 64)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(rightTopIndex));
+        {
+            var tile = Board.GetTileByIndex(rightTopIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //rightDown move
         int rightDownIndex = currentIndex - 6;
         if (currentIndex % 8 < 6 && rightDownIndex >= 0)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(rightDownIndex));
+        {
+            var tile = Board.GetTileByIndex(rightDownIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //DownLeft move
         int downLeftIndex = currentIndex - 17;
         if (currentIndex % 8 > 0 && downLeftIndex >= 0)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(downLeftIndex));
+        {
+            var tile = Board.GetTileByIndex(downLeftIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
         //DownLeft move
         int downRightIndex = currentIndex - 15;
         if (currentIndex % 8 < 7 && downRightIndex >= 0)
-            AddToBitboardIfNotOccupiedByFriend(ref bitboard, Board.GetTileByIndex(downRightIndex));
+        {
+            var tile = Board.GetTileByIndex(downRightIndex);
+            AddToBitboardIfNotOccupiedByFriend(ref bitboard, tile);
+            AttackingSquares.Add(tile.Bitboard);
+        }
 
-        MovingSquares = AttackingSquares = bitboard;
+        MovingSquares = bitboard;
     }
 
     private void AddToBitboardIfNotOccupiedByFriend(ref Bitboard bitboard, Tile tile) 
